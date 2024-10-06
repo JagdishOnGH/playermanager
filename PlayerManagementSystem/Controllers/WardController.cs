1cf26fcedc8543b295954ff4f813b6ca771e1eac
jagdish bhai <jagdissghimire@gmail.com>
2024-10-06 13:37:25 +0545

    ward controller added

--- PlayerManagementSystem/Controllers/WardController.cs
+++ PlayerManagementSystem/Controllers/WardController.cs
@@ -1,15 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlayerManagementSystem.EfContext;
+using PlayerManagementSystem.Helpers;
+using PlayerManagementSystem.Models;
 
 namespace PlayerManagementSystem.Controllers
 {
     [ApiController]
-    public class WardController : ControllerBase
+    [Route("/api/[controller]")]
+    public class WardController(EfDbContext context) : ControllerBase
     {
+        [Route("all")]
+        [HttpGet]
         public async Task<IActionResult> GetAllWards ()
         {
 
-            return Ok("hi");
-            //TODO to implement
+            try
+            {
+                var data = await context.Wards.ToListAsync();
+                return Ok(new ApiResponse<List<Ward>> { Data = data });
+
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new ApiResponse<string>
+                    {
+                        Error = e.Message,
+                        message = "Failed"
+                        
+                    })
+                ;
+            }
 
 
         }
