b4d777613fd22b995c194516abb553e0124fe9fa
jagdish bhai <jagdissghimire@gmail.com>
2024-11-11 16:06:58 +0545

    with teams

---
+++ PlayerManagementSystem/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace PlayerManagementSystem.Models;
+
+public class Person
+{
+    //person id first name last name email address roleId (enum) teamId (foreign key)
+    int PersonId { get; set; }  
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    string Email { get; set; }
+    Role Role { get; set; }
+    int TeamId { get; set; }
+    Team Team { get; set; }
+    
+}
+public enum Role
+{
+   Player = 1,
+    Coach = 2,
+    Manager = 3,
+   
+}
---
+++ PlayerManagementSystem/Models/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace PlayerManagementSystem.Models;
+
+public class Team
+{
+    int TeamId { get; set; }
+    string Name { get; set; }
+    string TerritoryType { get; set; }
+    int TerritoryId { get; set; } 
+}
