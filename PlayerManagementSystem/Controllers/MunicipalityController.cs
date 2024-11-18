bbae9c7ea08a6480b63f980bd786bf8c2e85c9b6
jagdish bhai <jagdissghimire@gmail.com>
2024-11-18 13:59:04 +0545

    Working in player for team

--- PlayerManagementSystem/Controllers/MunicipalityController.cs
+++ PlayerManagementSystem/Controllers/MunicipalityController.cs
@@ -2,13 +2,15 @@
 using Microsoft.EntityFrameworkCore;
 using PlayerManagementSystem.DTOs;
 using PlayerManagementSystem.EfContext;
+using PlayerManagementSystem.Helper;
 using PlayerManagementSystem.Helpers;
 using PlayerManagementSystem.Models;
 
 namespace PlayerManagementSystem.Controllers;
+
 [ApiController]
 [Route("api/[controller]")]
-public class MunicipalityController(EfDbContext context): ControllerBase
+public class MunicipalityController(EfDbContext context) : ControllerBase
 {
     [HttpGet]
     [Route("all")]
@@ -16,29 +18,17 @@ public class MunicipalityController(EfDbContext context): ControllerBase
     {
         try
         {
-            var districts = await context.Municipalities.Include(d=>d.District).ToListAsync();
-            var toReturn = new ApiResponse<List<Municipality>>
-            {
-                Data = districts,
-            };
+            var districts = await context.Municipalities.Include(d => d.District).ToListAsync();
+            var toReturn = new ApiResponse<List<Municipality>> { Data = districts };
             return Ok(toReturn);
-
         }
         catch (Exception e)
         {
-            var error = new ApiResponse<string>
-            {
-                Error = e.Message
-            };
+            var error = SharedHelper.CreateErrorResponse(e.Message);
             return NotFound(error);
-
-
-
         }
-        
-
     }
-    
+
     [HttpPost]
     [Route("add")]
     public async Task<IActionResult> AddDistrict(MunicipalsDto municipals)
@@ -46,62 +36,52 @@ public class MunicipalityController(EfDbContext context): ControllerBase
         try
         {
             //with province id check if province exists
-            var province = await context.Districts.FirstOrDefaultAsync(x => x.DistrictId == municipals.DistrictId);
+            var province = await context.Districts.FirstOrDefaultAsync(x =>
+                x.DistrictId == municipals.DistrictId
+            );
             if (province == null)
             {
-                var error = new ApiResponse<string>
-                {
-                    Error = "District does not exist"
-                };
+                var error = SharedHelper.CreateErrorResponse(
+                    $"District does not exist with id {municipals.DistrictId} "
+                );
                 return NotFound(error);
             }
             //also under same province check if district name or id exists
             var municipalExistsByName = await context.Municipalities.FirstOrDefaultAsync(x =>
-                x.Name.ToUpper() == municipals.MunipalityName.ToUpper() );
+                x.Name.ToUpper() == municipals.MunipalityName.ToUpper()
+            );
             if (municipalExistsByName != null)
             {
-                var error = new ApiResponse<string>
-                {
-                    Error = "Municipality already exists"
-                };
+                // var error = new ApiResponse<string> { Error = "Municipality already exists" };
+                var error = SharedHelper.CreateErrorResponse("Municipality already exists");
                 return BadRequest(error);
             }
-            
-            
-            
-            
 
             var newMun = new Municipality
             {
                 MunicipalityId = Guid.NewGuid(),
                 DistrictId = municipals.DistrictId,
-                Name = municipals.MunipalityName.ToUpper()
+                Name = municipals.MunipalityName.ToUpper(),
             };
-            context.Teams.Add(new Team
-            {
-                TeamId = Guid.NewGuid(),
-                Name = $"{municipals.MunipalityName}'s Team",
-                TerritoryId = newMun.MunicipalityId,
-                TerritoryType = TerritoryType.Municipality
-            });
+            context.Teams.Add(
+                new Team
+                {
+                    TeamId = Guid.NewGuid(),
+                    Name = $"{municipals.MunipalityName}'s Team",
+                    TerritoryId = newMun.MunicipalityId,
+                    TerritoryType = TerritoryType.Municipality,
+                }
+            );
             await context.Municipalities.AddAsync(newMun);
             await context.SaveChangesAsync();
 
             return Ok(newMun);
-
-          
-
         }
         catch (Exception e)
         {
-            var error = new ApiResponse<string>
-            {
-                Error = e.Message
-            };
+            //       var error = new ApiResponse<string> { Error = e.Message };
+            var error = SharedHelper.CreateErrorResponse(e.Message);
             return NotFound(error);
-
         }
-
     }
-    
 }
