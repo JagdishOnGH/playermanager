f52bf196eac33e8c463f9fc3e8ad1517c0ea8018
jagdish bhai <jagdissghimire@gmail.com>
2024-11-18 13:59:28 +0545

    Working in player for team

--- PlayerManagementSystem/Controllers/WardController.cs
+++ PlayerManagementSystem/Controllers/WardController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using PlayerManagementSystem.DTOs;
 using PlayerManagementSystem.EfContext;
+using PlayerManagementSystem.Helper;
 using PlayerManagementSystem.Helpers;
 using PlayerManagementSystem.Models;
 
@@ -9,104 +10,82 @@ namespace PlayerManagementSystem.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-
-public class WardController(EfDbContext context): ControllerBase
+public class WardController(EfDbContext context) : ControllerBase
 {
-     [HttpGet]
+    [HttpGet]
     [Route("all")]
     public async Task<IActionResult> GetAllMunicipals()
     {
         try
         {
-            var wards = await context.Wards.Include(w=>w.Municipality).ToListAsync();
-            var toReturn = new ApiResponse<List<Ward>>
-            {
-                Data = wards,
-            };
+            var wards = await context.Wards.Include(w => w.Municipality).ToListAsync();
+            var toReturn = new ApiResponse<List<Ward>> { Data = wards };
             return Ok(toReturn);
-
         }
         catch (Exception e)
         {
-            var error = new ApiResponse<string>
-            {
-                Error = e.Message
-            };
+            var error = SharedHelper.CreateErrorResponse(e.Message);
+
             return NotFound(error);
-
-
-
         }
-        
-
     }
-    
+
     [HttpPost]
     [Route("add")]
-    public async Task<IActionResult> AddDistrict(WardDto ward)
+    public async Task<IActionResult> AddWard(WardDto ward)
     {
         try
         {
             //with province id check if province exists
-            var municipal = await context.Municipalities.FirstOrDefaultAsync(x => x.MunicipalityId == ward.MunicipalityId);
+            var municipal = await context.Municipalities.FirstOrDefaultAsync(x =>
+                x.MunicipalityId == ward.MunicipalityId
+            );
             if (municipal == null)
             {
-                var error = new ApiResponse<string>
-                {
-                    Error = $"Municipality does not exist with id {ward.MunicipalityId} "
-                };
+                var error = SharedHelper.CreateErrorResponse(
+                    $"Municipality does not exist with id {ward.MunicipalityId} "
+                );
+
                 return NotFound(error);
             }
             //also under same province check if district name or id exists
             var wardNoExist = await context.Wards.FirstOrDefaultAsync(x =>
-                x.WardNo.ToUpper() == ward.WardNo.ToUpper() );
+                x.WardNo.ToUpper() == ward.WardNo.ToUpper()
+            );
+
             if (wardNoExist != null)
             {
-                var error = new ApiResponse<string>
-                {
-                    Error = $"Ward No  already exists with {ward.WardNo}"
-                };
-                return BadRequest(error);
+                var error = SharedHelper.CreateErrorResponse(
+                    $"Ward No  already exists with {ward.WardNo}"
+                );
+
+                return NotFound(error);
             }
 
-
-            
-            
-            
-            
-          
             var newWard = new Ward
             {
                 WardId = Guid.NewGuid(),
                 MunicipalityId = ward.MunicipalityId,
-                WardNo = ward.WardNo
+                WardNo = ward.WardNo,
             };
-          
-            context.Teams.Add(new Team
-            {
-                TeamId = Guid.NewGuid(),
-                Name = $"{ward.WardNo}'s Team",
-                TerritoryId = newWard.WardId,
-                TerritoryType = TerritoryType.Ward
-            }); 
-            await context.Wards.AddAsync(newWard); 
+
+            context.Teams.Add(
+                new Team
+                {
+                    TeamId = Guid.NewGuid(),
+                    Name = $"{municipal.Name} Ward No {ward.WardNo}'s Team",
+                    TerritoryId = newWard.WardId,
+                    TerritoryType = TerritoryType.Ward,
+                }
+            );
+            await context.Wards.AddAsync(newWard);
             await context.SaveChangesAsync();
             return Ok(newWard);
-
-          
-
         }
         catch (Exception e)
         {
-            var error = new ApiResponse<string>
-            {
-                Error = e.Message
-            };
+            var error = new ApiResponse<string> { Error = e.Message };
             return NotFound(error);
-
         }
-
     }
-    
-    
 }
