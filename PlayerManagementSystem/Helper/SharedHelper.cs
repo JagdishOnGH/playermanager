c9e55d586639df156e2d23dc559bc4453f8d9639
jagdish bhai <jagdissghimire@gmail.com>
2024-11-18 13:59:17 +0545

    Working in player for team

---
+++ PlayerManagementSystem/Helper/SharedHelper.cs
@@ -0,0 +1,11 @@
+﻿using PlayerManagementSystem.Helpers;
+
+namespace PlayerManagementSystem.Helper;
+
+public class SharedHelper
+{
+   public static  ApiResponse<string> CreateErrorResponse(string message)
+    {
+        return new ApiResponse<string> { Error = message };
+    }
+}
