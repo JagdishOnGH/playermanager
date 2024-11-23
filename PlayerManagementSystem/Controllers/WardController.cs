ccfe04ffa6a9afdc69e2ef3ff68ba063cba03c9a
jagdish bhai <jagdissghimire@gmail.com>
2024-11-23 19:39:33 +0545

    Add Wards moved to municipality controller

--- PlayerManagementSystem/Controllers/WardController.cs
+++ PlayerManagementSystem/Controllers/WardController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using PlayerManagementSystem.DTOs;
 using PlayerManagementSystem.EfContext;
@@ -31,83 +32,7 @@ public class WardController(EfDbContext context) : ControllerBase
         }
     }
 
-    [HttpPost]
-    [Route("addwards")]
-    [Authorize(AuthenticationSchemes = "Bearer")]
-    public async Task<IActionResult> AddWard(WardDto ward)
-    {
-        try
-        {
-            if (!User.HasClaim("Role", "Municipality"))
-            {
-                var error = SharedHelper.CreateErrorResponse(
-                    "You are not authorized to perform this action"
-                );
-                return BadRequest(error);
-            }
-            var tokenMunicipalId = User.Claims.FirstOrDefault(x => x.Type == "TerritoryId")?.Value;
-            if (tokenMunicipalId == null)
-            {
-                var error = SharedHelper.CreateErrorResponse("Municipality not found");
-                return BadRequest(error);
-            }
-
-            var validationErr = SharedHelper.ModelValidationCheck(ModelState);
-            if (validationErr != null)
-            {
-                var error = SharedHelper.CreateErrorResponse(validationErr);
-                return BadRequest(error);
-            }
-            //use muncipal id and check if same word no exists or not
-            var wardQuery = context
-                .Wards.Where(m => m.MunicipalityId == Guid.Parse(tokenMunicipalId))
-                .AsQueryable();
-            var wardNoExist = await wardQuery.FirstOrDefaultAsync(x => x.WardNo == ward.WardNo);
-
-            if (wardNoExist != null)
-            {
-                var error = SharedHelper.CreateErrorResponse(
-                    $"Ward No  already exists with {ward.WardNo}"
-                );
-
-                return BadRequest(error);
-            }
-            var myWard = new Ward
-            {
-                WardId = Guid.NewGuid(),
-                MunicipalityId = Guid.Parse(tokenMunicipalId),
-                WardNo = ward.WardNo,
-            };
-            var municipality = await context.Municipalities.FirstOrDefaultAsync(x =>
-                x.MunicipalityId == Guid.Parse(tokenMunicipalId)
-            );
-            if (municipality == null)
-            {
-                var error = SharedHelper.CreateErrorResponse(
-                    $"Municipality does not exist with id {tokenMunicipalId} "
-                );
-                return NotFound(error);
-            }
-
-            context.Teams.Add(
-                new Team
-                {
-                    TeamId = Guid.NewGuid(),
-                    Name = $"{municipality.Name} Ward No {ward.WardNo}'s Team",
-                    TerritoryId = myWard.WardId,
-                    TerritoryType = TerritoryType.Ward,
-                }
-            );
-            await context.Wards.AddAsync(myWard);
-            await context.SaveChangesAsync();
-            return Ok(myWard);
-        }
-        catch (Exception e)
-        {
-            var error = new ApiResponse<string> { Error = e.Message };
-            return NotFound(error);
-        }
-    }
+    
     [HttpGet]
     [Route("myteams")]
     [Authorize(AuthenticationSchemes = "Bearer")]
