63a64fbe4da4d5ef178a38d20825676f90c7e60a
jagdish bhai <jagdissghimire@gmail.com>
2024-11-24 20:44:09 +0545

    Now only authorized ward admin can add player

--- PlayerManagementSystem/Controllers/PersonDetailsController.cs
+++ PlayerManagementSystem/Controllers/PersonDetailsController.cs
@@ -14,10 +14,37 @@ public class PersonDetailsController(EfDbContext context) : ControllerBase
 {
     [HttpPost]
     [Route("add")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
     public async Task<IActionResult> AddPersonDetails(PersonDetailsDto personDetails)
     {
         try
         {
+            if(!User.HasClaim("Role", "Ward"))
+            {
+             //   var allClaimsinString = User.Claims.Select(x => x.Value).Aggregate((x, y) => x + " " + y);  
+                
+                var error = SharedHelper.CreateErrorResponse("You are not ward admin to perform this action ");
+                return BadRequest(error);
+            }
+            
+            var validationErr = SharedHelper.ModelValidationCheck(ModelState);
+            if (validationErr != null)
+            {
+                var error = SharedHelper.CreateErrorResponse(validationErr);
+                return BadRequest(error);
+            }
+            var tokenWardId = User.Claims.FirstOrDefault(x => x.Type == "TerritoryId")?.Value;
+            if (tokenWardId == null)
+            {
+                var error = SharedHelper.CreateErrorResponse("Ward not found");
+                return BadRequest(error);
+            }
+            var myTeam = await context.Teams.FirstOrDefaultAsync(x => x.TerritoryId == Guid.Parse(tokenWardId));
+            if (myTeam == null)
+            {
+                var error = SharedHelper.CreateErrorResponse("Team not found");
+                return BadRequest(error);
+            }
             var person = new Person
             {
                 FirstName = personDetails.FirstName,
